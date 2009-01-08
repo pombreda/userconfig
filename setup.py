dd90a43f1a2ac4d595f286593ae353158b49d62d
Pierre Raybaut <pr@pythonxy.com>
2009-01-08 18:18:33 +0100

    Setup refactoring

--- setup.py
+++ setup.py
@@ -28,29 +28,36 @@ This software is licensed under the terms of the GNU General Public
 License version 3 as published by the Free Software Foundation.
 """
 
+import userconfig as module
 import os.path as osp
+name = osp.split(osp.dirname(module.__file__))[-1]
+version = module.__version__
+packages = ['userconfig']
+package_data = {}
+description = 'Managing *easily* user configuration files (based on ConfigParser)'
+keywords = 'configuration file parser'
+classifiers = ['Development Status :: 5 - Production/Stable',
+               ]
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-import userconfig as module
-name = osp.split(osp.dirname(module.__file__))[-1]
-version = module.__version__
-
 setup(
       name = name,
       version = version,
-      description = 'Managing *easily* user configuration files (based on ConfigParser)',
+      description = description,
       download_url = 'http://%s.googlecode.com/files/%s-%s-py2.5.egg' % (name, name, version),
       author = "Pierre Raybaut",
       author_email = 'contact@pythonxy.com',
       url = 'http://code.google.com/p/%s/' % name,
       license = 'GPLv3',
-      keywords = 'configuration file parser',
+      keywords = keywords,
       platforms = ['any'],
-      packages = [name],
-      classifiers = ['Development Status :: 5 - Production/Stable',
+      packages = packages,
+      package_data = package_data,
+      classifiers = classifiers + [
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
